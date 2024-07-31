6997ba9179bdd74bef6c5c03fb2025c0cf57fa61
Ameer Shakeel <ameershakeelece@gmail.com>
2024-07-31 20:12:13 +0530

    Changed resource group name

--- lesson-01/main.tf
+++ lesson-01/main.tf
@@ -2,8 +2,8 @@
 # Please use the documentation here for reference:
 #   https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group#example-usage
 
-resource "azurerm_resource_group" "rajini" {
-  name     = "example"
+resource "azurerm_resource_group" "shakeel" {
+  name     = "shakeel"
   location = "East US"
 }
 
