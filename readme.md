d026afcaecd72937a3e7c6df5ab04c63d4403808
Ameer Shakeel <ameershakeelece@gmail.com>
2024-07-31 15:24:11 +0530

    First commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Learning Azure
+
+# To run terraform
+
+## Step 1 - To initialize Azure provider
+Run Terraform init:
+```
+terraform init
+```
+
+## Step 2 - To preview what will be done by Terraform
+Run Terraform plan:
+
+```
+terraform plan
+```
+
+### Step 3 - To create resources
+Run terraform apply:
+
+```
+terraform apply
+```
+
+### Step 4 - To delete all resources
+Run terraform destroy:
+
+```
+terraform destroy
+```
---
+++ vm/main.tf
@@ -0,0 +1,81 @@
+# Define the provider and the required version
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes      = ["10.0.1.0/24"]
+}
+
+# Create a public IP address
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Create a virtual machine
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.example.id]
+ 
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30 # Specify the size of the OS disk in GB
+  }
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub") # Path to your SSH public key
+  }
+}
+
+# Output the VM's public IP address
+output "public_ip" {
+  value = azurerm_public_ip.example.ip_address
+}
